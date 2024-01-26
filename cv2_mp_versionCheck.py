81442ff894d0969db1d5b9a4b0c0a0226716000d
Ethan Owen (mobile) <ethan.p.owen@gmail.com>
2024-01-25 17:26:53 -0800

    rev

--- cv2_mp_versionCheck.py
+++ cv2_mp_versionCheck.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
@@ -11,17 +10,3 @@ import pkg_resources
 installed_packages = pkg_resources.working_set
 installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
 print(installed_packages_list)
-=======
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-import math
-import numpy as np
-import cv2 as cv
-
-# Luis code to check installed package versions:
-import pkg_resources
-installed_packages = pkg_resources.working_set
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
-print(installed_packages_list)
->>>>>>> a27ef52d32210b9c1e5e34034adc24e8c7290d3b
